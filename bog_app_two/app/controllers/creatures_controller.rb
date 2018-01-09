99bf717766f800c4237dd57e203bbcdf055230a5
Xianyi555 <xianyi555@gmail.com>
2018-01-08 23:24:55 -0800

    finished the part 2 of Bog two

--- bog_app_two/app/controllers/creatures_controller.rb
+++ bog_app_two/app/controllers/creatures_controller.rb
@@ -2,4 +2,28 @@ class CreaturesController < ApplicationController
 	def index
 		@creatures = Creature.all
 	end
+
+	def new
+		@creature = Creature.new
+	end
+
+	def create
+      creature = Creature.create(creature_params)
+      redirect_to creature_path(creature)
+    end
+
+    # public methods up here
+    def show
+      @creature = Creature.find(params[:id])
+      puts("!!!!!!!!!!!!!!!!!!!!!!#{@creature}")
+    end
+
+    private
+
+    def creature_params
+      params.require(:creature).permit(:name, :description)
+    end
+
+
+
 end
