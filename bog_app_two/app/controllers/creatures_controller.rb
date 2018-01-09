e3cd332af238fd938534d815d26ef7b8e6f104bd
Xianyi555 <xianyi555@gmail.com>
2018-01-09 00:26:52 -0800

    finished Bog-two

--- bog_app_two/app/controllers/creatures_controller.rb
+++ bog_app_two/app/controllers/creatures_controller.rb
@@ -26,6 +26,12 @@ class CreaturesController < ApplicationController
     	redirect_to creature_path(@creature)
     end
 
+    def destroy
+    	@creature = Creature.find(params[:id])
+    	@creature.destroy
+    	redirect_to creatures_path
+    end
+
     private
 
     def creature_params
--- bog_app_two/config/routes.rb
+++ bog_app_two/config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
   get "/creatures", to: "creatures#index", as: "creatures"
   get "/creatures/new", to: "creatures#new", as: "new_creatures"
   post "/creatures", to: "creatures#create"
-  get "creatures/:id", to: "creatures#show", as: "creature"
-  get "creatures/:id/edit", to: "creatures#edit", as: "edit_creature"
+  get "/creatures/:id", to: "creatures#show", as: "creature"
+  get "/creatures/:id/edit", to: "creatures#edit", as: "edit_creature"
   patch "/creatures/:id", to: "creatures#update"
+  delete "/creatures/:id", to: "creatures#destroy"
 
 
 end
