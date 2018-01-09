931d21b4784126e31d2e1268df9ddc0142114b1c
Xianyi555 <xianyi555@gmail.com>
2018-01-09 00:04:03 -0800

    finished Part 4 for Bog-Two

--- bog_app_two/app/controllers/creatures_controller.rb
+++ bog_app_two/app/controllers/creatures_controller.rb
@@ -8,22 +8,32 @@ class CreaturesController < ApplicationController
 	end
 
 	def create
-      creature = Creature.create(creature_params)
-      redirect_to creature_path(creature)
+        @creature = Creature.create(creature_params)
+        redirect_to creature_path(@creature)
     end
 
-    # public methods up here
     def show
-      @creature = Creature.find(params[:id])
-      puts("!!!!!!!!!!!!!!!!!!!!!!#{@creature}")
+        @creature = Creature.find(params[:id])
+    end
+
+    def edit
+    	@creature = Creature.find(params[:id])
+    end
+
+    def update
+    	@creature = Creature.find(params[:id])
+    	@creature.update_attributes(creature_params)
+    	redirect_to creature_path(@creature)
     end
 
     private
 
     def creature_params
-      params.require(:creature).permit(:name, :description)
+        params.require(:creature).permit(:name, :description)
     end
 
 
 
 end
+
+
--- bog_app_two/config/routes.rb
+++ bog_app_two/config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   get "/creatures/new", to: "creatures#new", as: "new_creatures"
   post "/creatures", to: "creatures#create"
   get "creatures/:id", to: "creatures#show", as: "creature"
+  get "creatures/:id/edit", to: "creatures#edit", as: "edit_creature"
+  patch "/creatures/:id", to: "creatures#update"
+
 
 end
